e68c888d52c658cb496d29ee019235bf3a4d25ff
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2019-01-15 00:02:01 +0900

    complete naive implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,90 +1,107 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import qualified Data.ByteString as ByteString
-import Data.Digest.Pure.MD5
-import Data.Foldable
-import Data.Time
-import Graphics.HsExif
-import System.FilePath
-import System.Directory
-import qualified Data.Serialize as Cereal
-import qualified Web.Pixela as Pixela
-import qualified Data.Yaml as Yaml
-import Options.Declarative
-import Data.Version (showVersion)
-import Control.Monad.IO.Class
+import           Control.Exception       hiding (Exception)
+import qualified Control.Exception       as E
+import           Control.Lens
+import           Control.Monad.IO.Class
+import           Data.Aeson.Lens
+import qualified Data.ByteString         as ByteString
+import           Data.Digest.Pure.MD5
+import           Data.Foldable
+import           Data.Maybe
+import qualified Data.Serialize          as Cereal
+import qualified Data.Text               as Text
+import           Data.Time
+import           Data.Version            (showVersion)
+import           Graphics.HsExif
+import           Options.Declarative
+import           System.Directory
+import           System.FilePath
+import           System.IO
+import           Text.Read               (readMaybe)
+import           Type.Reflection
+import qualified Web.Pixela              as Pixela
 
-import Paths_photograph_pixela (version)
+import           Paths_photograph_pixela (version)
 
-import Config (Config (Config), readConfig)
+import           Config                  (Config (Config, pixela), readConfig)
 
 main :: IO ()
 main =
   run "photograph-pixela" (Just $ showVersion version) command
 
 command
-  :: Flag "c" '["config"] "FILE" "configuration file. default \".photography-pixela.yaml\"" (Def ".photography-pixela.yaml" String)
-  -> Flag "s" '["cache"] "DIR" "cache directory. default \".photography-pixela\"" (Def ".photography-pixela" String)
+  :: Flag "c" '["config"] "FILE" "configuration file. default \".photograph-pixela.yaml\"" (Def ".photograph-pixela.yaml" String)
+  -> Flag "s" '["cache"] "DIR" "cache directory. default \".photograph-pixela\"" (Def ".photograph-pixela" String)
   -> Cmd "Upload shot dates to Pixela" ()
 command configPath cachePath =
   liftIO $ do
-    config <- readConfig $ get configPath
-    doDirectory config (get cachePath) "."
+    config@Config { pixela = pixelaConfig } <- readConfig $ get configPath
+    pixelaClient <- Pixela.newClient pixelaConfig
+    doDirectory config pixelaClient (get cachePath) "."
 
-doDirectory :: Config -> FilePath -> FilePath -> IO ()
-doDirectory config cacheDir dir = do
+doDirectory :: Config -> Pixela.Client -> FilePath -> FilePath -> IO ()
+doDirectory config pixelaClient cacheDir dir = do
   paths <- listDirectory dir
-  (dirs, files) <- dirFile dir paths
+  (allDirs, files) <- dirFile dir paths
   let
     photos = filter ((`elem` [".ARW", ".JPG"]) . takeExtension) files
-  traverse_ (doFile config cacheDir dir) photos
-  traverse_ (doDirectory config cacheDir) dirs
+    dirs = filter (/= cacheDir) allDirs
+  traverse_ (doFile config pixelaClient cacheDir dir) photos
+  traverse_ (doDirectory config pixelaClient cacheDir . (dir </>)) dirs
 
-doFile :: Config -> FilePath -> FilePath -> FilePath -> IO ()
-doFile (Config timeZone _ _) cacheDir dir filePath = do
-  let
-    cache = cacheDir </> dir </> filePath
-    photo = dir </> filePath
-  cached <- doesFileExist $ cache
-  if cached
-    then do
-      putStrLn $ "cached: " <> dir </> filePath
-      cachedTime <- getModificationTime cache
-      print cachedTime
-      fileTime <- getModificationTime photo
-      print fileTime
-      originalTime <- zonedTimeToUTC . (\lt -> ZonedTime lt timeZone) <$> exifDateTimeOriginal photo
-      print originalTime
-      let originalDay = utctDay originalTime
-      if fileTime <= cachedTime
-        then putStrLn "nothing to do"
-        else do
-          -- modified after the previous check
-          -- compare file hashes
-          putStrLn "modified after the previous check"
-          hashValue <- hash' <$> ByteString.readFile (dir </> filePath) :: IO MD5Digest
-          print hashValue
-          prevHashValueOrErro <- Cereal.decode <$> ByteString.readFile cache
-          case prevHashValueOrErro of
-            Right prevHashValue -> do
-              print prevHashValue
-              if hashValue == prevHashValue
-                then do
-                  putStrLn "update cached modificationtime"
-                  setModificationTime cache fileTime
-                else do
-                  putStrLn "TODO: upload"
-                  ByteString.writeFile cache $ Cereal.encode hashValue
-            Left err -> error err
-    else do
-      putStrLn $ "not cached: " <> dir </> filePath
-      hashValue <- hash' <$> ByteString.readFile (dir </> filePath) :: IO MD5Digest
-      print hashValue
-      putStrLn "TODO: upload"
-      createDirectoryIfMissing True $ cacheDir </> dir
-      ByteString.writeFile cache $ Cereal.encode hashValue
+doFile :: Config -> Pixela.Client -> FilePath -> FilePath -> FilePath -> IO ()
+doFile (Config timeZone pixelaGraphId _) pixelaClient cacheDir dir filePath =
+  do
+    let
+      cache = cacheDir </> dir </> filePath
+      photo = dir </> filePath
+    putStrLn $ dir </> filePath
+    originalTime <- zonedTimeToUTC . (`ZonedTime` timeZone) <$> exifDateTimeOriginal photo
+    let
+      originalDay = utctDay originalTime
+    cached <- doesFileExist cache
+    if cached
+      then do
+        putStrLn "  cached"
+        cachedTime <- getModificationTime cache
+        putStrLn $ "  cached time:             " <> show cachedTime
+        fileTime <- getModificationTime photo
+        putStrLn $ "  file time:               " <> show fileTime
+        putStrLn $ "  EXIF Date/Time Original: " <> show originalTime
+        if fileTime <= cachedTime
+          then putStrLn "  nothing to do"
+          else do
+            -- modified after the previous check
+            -- compare file hashes
+            putStrLn "modified after the previous check"
+            hashValue <- hash' <$> ByteString.readFile (dir </> filePath) :: IO MD5Digest
+            prevHashValueOrErro <- Cereal.decode <$> ByteString.readFile cache
+            case prevHashValueOrErro of
+              Right prevHashValue ->
+                if hashValue == prevHashValue
+                  then do
+                    putStrLn "  update cached modification time"
+                    setModificationTime cache fileTime
+                  else do
+                    updatePixela pixelaGraphId originalDay pixelaClient
+                    putStrLn "  uploaded"
+                    ByteString.writeFile cache $ Cereal.encode hashValue
+              Left err -> throw $ Exception err
+      else do
+        putStrLn "  not cached"
+        putStrLn $ "  EXIF Date/Time Original: " <> show originalTime
+        hashValue <- hash' <$> ByteString.readFile (dir </> filePath) :: IO MD5Digest
+        updatePixela pixelaGraphId originalDay pixelaClient
+        putStrLn "  uploaded"
+        createDirectoryIfMissing True $ cacheDir </> dir
+        ByteString.writeFile cache $ Cereal.encode hashValue
+  `catch` \e -> do
+    hPutStrLn stderr $ displayException (e :: Exception)
+    logFile $ "[" <> dir </> filePath <> "] " <> displayException e
 
 dirFile :: FilePath -> [FilePath] -> IO ([FilePath], [FilePath])
 dirFile dir = go ([], [])
@@ -102,5 +119,26 @@ dirFile dir = go ([], [])
 
 exifDateTimeOriginal :: FilePath -> IO LocalTime
 exifDateTimeOriginal path = do
-  exif <- either error id <$> parseFileExif path
-  maybe (error "no Data/Time Original") pure $ getDateTimeOriginal exif
+  exif <- either (throw . Exception) id <$> parseFileExif path
+  maybe (throw $ Exception "no Date/Time Original") pure $ getDateTimeOriginal exif
+
+updatePixela :: Pixela.GraphName -> Day -> Pixela.Client -> IO ()
+updatePixela graphName day client = do
+  mq <- (((>>= readMaybe . Text.unpack) . (^? key "quantity" . _String)) <$> Pixela.getQuantity graphName day client) `catch` ((\_ -> pure $ Just 0) :: SomeException -> IO (Maybe Int))
+  let
+    q = fromMaybe (throw $ Exception "can't parse as integer") mq
+  Pixela.updateQuantity graphName day (show $ q + 1) client
+
+logFile :: String -> IO ()
+logFile message = do
+  tz <- getCurrentTimeZone
+  utct <- getCurrentTime
+  let
+    time = utcToLocalTime tz utct
+    timeString = formatTime defaultTimeLocale "%F %T" time
+  appendFile ".photograph-pixela.log" $ "[" <> timeString <> "] " <> message <> "\n"
+
+newtype Exception = Exception String deriving (Show, Typeable)
+
+instance E.Exception Exception where
+  displayException (Exception message) = message
--- src/PhotographPixela.hs
+++
@@ -1,6 +0,0 @@
-module PhotographPixela
-    ( someFunc
-    ) where
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
